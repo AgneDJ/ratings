8387db81d1737a0bbd321218896a3d1aadee6cbe
AgneDJ <fizikee@gmail.com>
2023-12-05 00:19:12 -0800

    First part of excercise done

--- ratings.py
+++ ratings.py
@@ -2,3 +2,29 @@
 
 
 # put your code here
+def rating(filename):
+    restaurant = input("What is your restaurant?")
+    rating = input()
+
+    opened = open(filename, "a")
+    file = opened.write(restaurant)
+    file = opened.write(rating)
+
+    ratings = {}
+
+    for line in file:
+        line = line.rstrip()
+        items = line.split(':')
+        first_item = items[0]
+        second_item = int(items[1])
+
+        ratings[first_item] = second_item
+        something = ratings.items()
+
+    type(something)
+    something = sorted(something)
+    print(something)
+
+
+filename = "scores.txt"
+rating(filename)
